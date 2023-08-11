7f4427487d87c37f65712c1b1ef770c0a95e9c51
Nayalish <76243048+Nayalish@users.noreply.github.com>
2023-08-11 20:18:24 +0500

    Add files via upload

---
+++ src/routes/Process.js
@@ -0,0 +1,21 @@
+import Serachcomponent from "../components/Serachcomponent";
+
+function Process() {
+  return (
+    <>
+      {/* <Main_section
+      clasname="Main_section-mid"
+      imgurl={AboutImg}
+      // text="Yous Journey Your Story"
+      title="Process"
+      // url= "/"
+      // btnClass="show"
+      // buttonText="Travel plan"
+      /> */}
+      <Serachcomponent/>
+      {/* <Footer/> */}
+    </>
+  );
+}
+
+export default Process;
